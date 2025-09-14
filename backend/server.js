59706ef4f7e18c5cad0d4cc2cafb6daaeff304fb
abdull-07 <personal.muhammadabdullah@gmail.com>
2025-09-14 21:42:19 +0500

    fix cors error

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production'
     ? process.env.FRONTEND_URL
-    : ['http://localhost:5173', 'http://localhost:5174'],
+    : 'https://meddesk-nhqt.onrender.com',
   credentials: true,
   optionsSuccessStatus: 200
 };
